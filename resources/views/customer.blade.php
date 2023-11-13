cfbcfb119f1fbc5edffa4e19ffe983f71ca99618
JonWendell <144750558+JonWendell@users.noreply.github.com>
2023-11-13 20:05:12 +0800

    main

---
+++ resources/views/customer.blade.php
@@ -0,0 +1,5 @@
+@extends('ecom.front.ecom-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Page Title here')
+@section('content')
+    content here...
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 
 Route::view('/login-auth','login-auth');
 Route::view('/example-page','example-page');
+Route::view('/customer','customer');

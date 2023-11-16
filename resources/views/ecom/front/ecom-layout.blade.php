0c7feed5d318540bf60fe097a7ba4ea70ca0256a
JonWendell <jerlantupas4@gmail.com>
2023-11-16 12:11:19 +0800

    main

--- resources/views/ecom/front/ecom-layout.blade.php
+++ resources/views/ecom/front/ecom-layout.blade.php
@@ -160,31 +160,31 @@
             <p class="price"><del>95.00</del> &mdash; $55.00</p>
           </div>
           <div class="col-sm-6 col-lg-4 text-center item mb-4">
-            <a href="shop-single.html"> <img src="/back/images/product_02.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Chanca Piedra</a></h3>
+            <a href="/ordered"> <img src="/back/images/product_02.png" alt="Image"></a>
+            <h3 class="text-dark"><a href="/ordered">Chanca Piedra</a></h3>
             <p class="price">$70.00</p>
           </div>
           <div class="col-sm-6 col-lg-4 text-center item mb-4">
-            <a href="shop-single.html"> <img src="/back/images/product_03.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
+            <a href="/ordered"> <img src="/back/images/product_03.png" alt="Image"></a>
+            <h3 class="text-dark"><a href="/ordered">Umcka Cold Care</a></h3>
             <p class="price">$120.00</p>
           </div>
 
           <div class="col-sm-6 col-lg-4 text-center item mb-4">
 
-            <a href="shop-single.html"> <img src="/back/images/product_04.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Cetyl Pure</a></h3>
+            <a href="/ordered"> <img src="/back/images/product_04.png" alt="Image"></a>
+            <h3 class="text-dark"><a href="/ordered">Cetyl Pure</a></h3>
             <p class="price"><del>45.00</del> &mdash; $20.00</p>
           </div>
           <div class="col-sm-6 col-lg-4 text-center item mb-4">
-            <a href="shop-single.html"> <img src="/back/images/product_05.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">CLA Core</a></h3>
+            <a href="/ordered"> <img src="/back/images/product_05.png" alt="Image"></a>
+            <h3 class="text-dark"><a href="/ordered">CLA Core</a></h3>
             <p class="price">$38.00</p>
           </div>
           <div class="col-sm-6 col-lg-4 text-center item mb-4">
             <span class="tag">Sale</span>
-            <a href="shop-single.html"> <img src="/back/images/product_06.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Poo Pourri</a></h3>
+            <a href="/ordered"> <img src="/back/images/product_06.png" alt="Image"></a>
+            <h3 class="text-dark"><a href="/ordered">Poo Pourri</a></h3>
             <p class="price"><del>$89</del> &mdash; $38.00</p>
           </div>
         </div>

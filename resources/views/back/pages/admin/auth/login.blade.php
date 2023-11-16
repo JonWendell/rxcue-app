c0f4cbc8e2e19b7a8049f517514d9c6fcf291c4d
cydieg <cydiegargullo@gmail.com>
2023-11-16 14:23:53 +0800

    main

--- resources/views/back/pages/admin/auth/login.blade.php
+++ resources/views/back/pages/admin/auth/login.blade.php
@@ -1,24 +1,48 @@
 @extends('back.layout.auth-layout')
-@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Page Title here')
+
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Admin login')
+
 @section('content')
 <div class="login-box bg-white box-shadow border-radius-10">
     <div class="login-title">
-        <h2 class="text-center text-primary">Login To DeskApp</h2>
+        <h2 class="text-center text-primary">Admin login</h2>
     </div>
-    <form>
-       
-        <div class="input-group custom">
-            <input type="text" class="form-control form-control-lg" placeholder="Username">
-            <div class="input-group-append custom">
-                <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+    <form action="{{ route('admin.login_handler') }}" method="POST">
+        @csrf
+        @if (Session::get('fall'))
+            <div class="alert alert-danger">
+                {{ Session::get('fall') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-        </div>
+        @endif
+
         <div class="input-group custom">
-            <input type="password" class="form-control form-control-lg" placeholder="********">
+            <input type="text" class="form-control form-control-lg" placeholder="Email/Username" name="login_id" value="{{ old('login_id') }}">
+            <div class="input-group-append custom">
+                <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></div>
+        </div>
+
+        @error('login_id')
+            <div class="d-block text-danger" style="margin-top: -25px; margin-bottom: -25px">
+                {{ $message }}
+            </div>
+        @enderror
+
+        <div class="input-group custom">
+            <input type="password" class="form-control form-control-lg" placeholder="********" name="password">
             <div class="input-group-append custom">
                 <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
             </div>
         </div>
+
+        @error('password')
+            <div class="d-block text-danger" style="margin-top: -25px; margin-bottom: -25px">
+                {{ $message }}
+            </div>
+        @enderror
+
         <div class="row pb-30">
             <div class="col-6">
                 <div class="custom-control custom-checkbox">
@@ -35,10 +59,7 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="input-group mb-0">
-                    <!--
-                    use code for form submit
-                    <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
-                -->
+                    
                     <a class="btn btn-primary btn-lg btn-block" href="index.html">Sign In</a>
                 </div>
             </div>
